eec1c76aa7d9d0027358c087bd931113f130e229
Tim Coats <trcoats126@gmail.com>
2016-04-06 15:22:06 -0400

    Create Rudimentary skeleton of main backend Switching class

--- src/edu/bsu/cs327/Switching.java
+++ src/edu/bsu/cs327/Switching.java
@@ -1,26 +1,51 @@
 package edu.bsu.cs327;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Switching {
+    ArrayList<HashMap<String, Integer>>switchTable = new ArrayList<HashMap<String, Integer>>();
+
+    public String handleInput(String srcMacAddress, int portNumber, String destMacAddress){
+        String output = "";
+        HashMap tableEntry = new HashMap();
+        tableEntry.put(srcMacAddress, portNumber);
+        switchTable.add(tableEntry);
+        for (HashMap hm : switchTable) {
+            if (hm.containsKey(destMacAddress)) {
+                output = hm.get(destMacAddress).toString();
+            } else {
+                output = "Ports flooded";
+            }
+        }
+        return output;
+    }
+
+//    public String handleDestInput(String destMacAddress) {
+//        String output = "";
+//        for (HashMap hm : switchTable) {
+//            if (hm.containsKey(destMacAddress)) {
+//                output = hm.get(destMacAddress).toString();
+//            } else {
+//                output = "blah";
+//            }
+//        }
+//        return output;
+//    }
+
     public String frameErrorCheck(String macAddress){
         String output = "";
         if(macAddress.length() != 6){
             output += "Error: MAC address must be 6 bytes!";
         }
-        else{
-            //run program
-        }
         return output;
     }
 
-    public String portErrorCheck(int portNumber){
+    public String portErrorCheck(int portNumber) {
         String output = "";
-        if (portNumber > 24){
+        if (portNumber > 24) {
             output += "Error: Port# not supported!";
         }
-        else{
-            //runProgram
-        }
         return output;
     }
-
 }
